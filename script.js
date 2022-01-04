36d774f6ba76e235e00617f90a5927b2665dc1fe
AmmarKhawaja <ammarkhawaja01@gmail.com>
2022-01-04 17:11:24 -0500

    Edited files

--- script.js
+++ script.js
@@ -4,14 +4,37 @@ box.setAttribute("id","box");
 box.classList.add("box");
 box.textContent = "Hi";
 container.appendChild(box);
-for (let i = 0; i < 10; i++) {
-    var copy = box.cloneNode(true);
-    container.appendChild(copy);
-}
 
-const a = document.querySelectorAll("#box");
-a.forEach((elem) => {
+const row = document.createElement("div");
+row.setAttribute("id","row");
+row.classList.add("row");
+row.appendChild(box);
+container.appendChild(row)
+
+
+function createBoxes(length) {
+    for (let i = 0; i < length; i++) {
+        var copy = box.cloneNode(true);
+        container.appendChild(copy);
+        
+    }
+    
+}
+function addBoxListeners() {
+    const a = document.querySelectorAll("#box");
+    a.forEach((elem) => {
     elem.addEventListener("mouseover",function(e) {
         e.target.style.backgroundColor = ("gray");
+        })
     })
-})
+}
+function buttonClicked() {
+    //document.getElementById("box").remove();
+    const boxes = document.querySelectorAll("#box")
+    boxes.forEach((elem) => {
+        elem.remove();
+    })
+    const input = prompt("Enter side length.")
+    createBoxes(input);
+    addBoxListeners();
+}
